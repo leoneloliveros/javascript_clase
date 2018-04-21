5deb721d40bc8a5bdfc504d458e92a79963edfc1
CFV <cristian.fraile@morningstar.com>
2018-04-20 20:35:57 -0500

    Ejercicio 7

--- contar/app.js
+++ contar/app.js
@@ -1,18 +1,8 @@
-function contar(str){
-	// var a = 0;
-	// var size = str.length;
-	// for (var i=0; i < size; i++) {
-	// 	if (str.charAt(i) === "a") {
-	// 		a++;			
-	// 	}
-	// }
-	var a = str.match(/a/g);
+function contar(str, char ){
+	var a = str.match(new RegExp(char,"g"));
 	return a === null ? 0 : a.length;
-	// if (a === null) {
-	// 	return 0;
-	// } else {
-	// 	return a.length;
-	// }
+
 }
-console.log(contar("Hola Make it Real")); // 3
-console.log(contar("")); // 0
+
+console.log(contar("Hola Make it Real", "i")); // 1
+console.log(contar("Hello Worldooooooooooooo", "o")); // 3

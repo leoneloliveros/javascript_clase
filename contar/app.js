5c90a8f0644d59b5197bc914235d4123cb527819
Leonel Oliveros <leoneloliveros.co@gmail.com>
2018-04-20 20:21:02 -0500

    Cambios de ejercicio 1

--- contar/app.js
+++ contar/app.js
@@ -1,12 +1,17 @@
 function contar(str){
-	var a = 0;
-	var size = str.length;
-	for (var i=0; i < size; i++) {
-		if (str.charAt(i) === "a") {
-			a++;			
-		}
+	// var a = 0;
+	// var size = str.length;
+	// for (var i=0; i < size; i++) {
+	// 	if (str.charAt(i) === "a") {
+	// 		a++;			
+	// 	}
+	// }
+	var a = str.match(/a/g);
+	if (a === null) {
+		return 0;
+	} else {
+		return a.length;
 	}
-	return a;
 }
-console.log(contar("HolA Make it Real")); // 3
+console.log(contar("Hola Make it Real")); // 3
 console.log(contar("")); // 0
